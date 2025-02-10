63a2c1bb4685a8c9b1f6787a8dfa385b0248fcfb
Muhammad Aarib <106152587+AribDatabeys@users.noreply.github.com>
2025-02-10 11:54:56 +0500

    Delete

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const expressLayout = require('express-ejs-layouts')
-const app  = express();
-app.use(expressLayout);
-app.set('view engine','ejs')
-app.use('/',require(`./routes/index`))
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server run on ${PORT}`))
